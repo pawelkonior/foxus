1612bf8aea11113a521a17e345e3e2bc18d94a95
Pawel Konior <pawel.konior@gmail.com>
2020-04-24 20:16:36 +0200

    add material theme ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
+import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles';
+import {orange} from '@material-ui/core/colors';
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+// import CustomCheckbox from "./components/checkbox";
+// import CameraStream from "./components/CameraStream";
+// import Dashboard from "./components/Dashboard";
+
+const theme = createMuiTheme({
+  status: {
+    danger: orange[500],
+  },
+  palette: {
+    primary: {
+      main: '#ff4400',
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      contrastText: '#ffcc00',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontSize: "10px"
+  }
+});
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    position: "relative",
+    maxWidth: "850px",
+    width: "100%",
+    margin: "auto"
+  }
+}));
+
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Foqs</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.main}>
+        <LinearProgress/>
+        <h1>Foqs</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
